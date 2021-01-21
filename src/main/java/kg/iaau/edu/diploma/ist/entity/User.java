eace89e447c1631cbf43f4e5e3d57a653d6c6f9f
Tilek Dzhusukeev <Tilekdzhusukeev@gmail.com>
2021-01-21 15:56:01 +0600

    User was added

---
+++ src/main/java/kg/iaau/edu/diploma/ist/entity/User.java
@@ -0,0 +1,85 @@
+package kg.iaau.edu.diploma.ist.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "USER_FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "USER_LAST_NAME")
+    private String lastName;
+
+    @Column(name = "USER_EMAIL")
+    private String email;
+
+    @Column(name = "USER_PASSWORD")
+    private String password;
+
+    @Column(name = "USER_ACTIVE")
+    private Boolean active;
+
+    @Column(name = "CREATED_DATE")
+    private Date date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
